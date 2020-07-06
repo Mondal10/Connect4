0113667cb7ca3c93ef9673316a674860fafc4b2d
Mondal10 <amitmondal111.am@gmail.com>
2020-07-06 23:53:57 +0530

    Updates

---
+++ precache-manifest.e6ac485b9cc7f461a9dcb8f7cabbbdc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c12c7edc3cedece729c56f0ad75c782",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c9efc53650b8d41cec5",
+    "url": "/static/css/main.79a57d20.chunk.css"
+  },
+  {
+    "revision": "afd239e5bd670970551f",
+    "url": "/static/js/2.f16db2bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f16db2bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c9efc53650b8d41cec5",
+    "url": "/static/js/main.cc0ff744.chunk.js"
+  },
+  {
+    "revision": "af8b80d9e4d2da71a414",
+    "url": "/static/js/runtime-main.7fef498f.js"
+  },
+  {
+    "revision": "d10b9ad2f219f337692b94d38fe6f701",
+    "url": "/static/media/4inarow.d10b9ad2.png"
+  }
+]);
